843e6401602677c20f8356dd00b9f073f2041128
David Muriithi <muriithid05@gmail.com>
2024-01-25 18:24:17 +0300

    modified:   saver.py

--- powersaver/saver.py
+++ powersaver/saver.py
@@ -1,44 +1,37 @@
-import subprocess
+#!/usr/bin/python3
+
 import platform
+import subprocess
 
 def check_status() -> bool:
-    """
-    Check if system is running on battery.
-
-    Returns:
-        bool: True if running on battery, False otherwise.
-    """
-
     try:
-        battery_status = subprocess.check_output(["upower", "-i", "/org/freedesktop/Upower/devices/battery_BAT0"],
-        universal_newlines=True,
-        )
-        return "state: discharging" in battery_status.lower()
-    except subprocess.CalledProcessError as e:
+        with open("/sys/class/power_supply/BAT0/status", "r") as status_file:
+            battery_status = status_file.read().strip().lower()
+        print(f"Battery Status: {battery_status}")
+        return battery_status == "discharging"
+    except FileNotFoundError as e:
         print(f"Error checking power status: {e}")
         return False
 
-def set_mode(mode: str) -> None:
-    """
-    Set power mode (Ubuntu)
-
-    Args:
-        mode(str): The desired power mode
-    """
+def set_power_saving_mode() -> None:
     try:
         if platform.system() == "Linux":
             subprocess.run(
-                    ["gsettings", "set", "org.gnome.settings-daemon.plugins.power", "profile", mode],
-                    check=True,
+                ["sudo", "tlp", "start"],
+                check=True,
+                stdout=subprocess.PIPE, stderr=subprocess.PIPE
             )
-            print(f"Power mode set to '{mode}")
+            print("Power-saving mode activated.")
         else:
             print("Your operating system is not supported")
     except subprocess.CalledProcessError as e:
-        print(f"Error setting power mode: {e}")
+        print(f"Error setting power-saving mode: {e}")
+        print(f"Command that failed: {e.cmd}")
+        print(f"Error output: {e.stderr.decode('utf-8')}")
 
 if __name__ == "__main__":
     if check_status():
-        set_mode("balanced")
+        set_power_saving_mode()
     else:
         print("PC is plugged in. No action needed.")
+
